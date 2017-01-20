5ce69b5e6be4e137fa1ff545d77f4c20b2147391
somebody <some@body.com>
2017-01-20 13:35:52 +0300

    first test

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,6 +1,11 @@
 package main;
 
+import org.hibernate.SessionFactory;
+
 public class Main
 {
-
+	public static void main(String[] args)
+	{
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+	}
 }
